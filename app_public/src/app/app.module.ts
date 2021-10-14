a8ea32761aa86bfcdd31eab3c5a34dd091340a18
Ife Jeremiah <jerryroboy.16@gmail.com>
2021-10-14 09:11:59 +0100

    removed angular 12.0, replaced with angular 7.0, for study purposes

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { HomeListComponent } from './home-list/home-list.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    HomeListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [HomeListComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
