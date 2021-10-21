6c281403569aa923abf0181ef496095b8e3c0332
Ife Jeremiah <jerryroboy.16@gmail.com>
2021-10-21 17:12:15 +0100

    Built Single Page Application with Angular ready for integration to main application

--- app_public/src/app/loc8r-data.service.ts
+++ app_public/src/app/loc8r-data.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { Location } from './home-list/home-list.component';
+import { environment } from '../environments/environment';
+import { Location, Review } from './location';
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +11,39 @@ export class Loc8rDataService {
 
   constructor(private http: HttpClient) { }
 
-  private apiBaseUrl = 'http://localhost:8000/api';
+  private handleError(error: any): Promise<any> {
+    console.error('Something has gone wrong', error);
+    return Promise.reject(error.message || error);
+  }
+
+  private apiBaseUrl: string = environment.apiBaseUrl;
+
   public getLocations(lat: number, lng: number): Promise<Location[]> {
     const maxDistance: number = 2000000;
-
     const url: string = `${this.apiBaseUrl}/locations?lng=${lng}&lat=${lat}&maxdistance=${maxDistance}`;
-
     return this.http
       .get(url)
       .toPromise()
       .then(response => response as Location[])
       .catch(this.handleError);
   }
-  private handleError(error: any): Promise<any> {
-    console.error('Something has gone wrong', error);
-    return Promise.reject(error.message || error);
+
+  public getLocationById(locationid: string): Promise<Location> {
+    const url: string = `${this.apiBaseUrl}/locations/${locationid}`;
+    return this.http
+      .get(url)
+      .toPromise()
+      .then(response => response as Location)
+      .catch(this.handleError);
+  }
+
+  public addReviewByLocationId(locationid: string, formData: Review): Promise<Review> {
+    const url = `${this.apiBaseUrl}/locations/${locationid}/reviews`;
+    return this.http
+      .post(url, formData)
+      .toPromise()
+      .then(response => response as Review)
+      .catch(this.handleError);
   }
 
 }
---
+++ app_public/src/app/location-details/location-details.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Location, Review } from '../location';
+import { Loc8rDataService } from '../loc8r-data.service';
+
+@Component({
+  selector: 'app-location-details',
+  templateUrl: './location-details.component.html',
+  styleUrls: ['./location-details.component.css']
+})
+export class LocationDetailsComponent implements OnInit {
+  @Input() location: Location;
+
+  public googleAPIKey: string = '<Put Your Google Maps API Key Here>';
+
+  public newReview: Review = {
+    author: '',
+    rating: 5,
+    reviewText: ''
+  };
+
+  public formVisible: boolean = false;
+
+  public formError: string;
+
+  private formIsValid(): boolean {
+    if (this.newReview.author && this.newReview.rating && this.newReview.reviewText && !(this.newReview.author === ' ') && !(this.newReview.reviewText === ' ')) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  private resetAndHideReviewForm(): void {
+    this.formVisible = false;
+    this.newReview.author = '';
+    this.newReview.rating = 5;
+    this.newReview.reviewText = '';
+  };
+
+  public onReviewSubmit(): void {
+    this.formError = '';
+    if (this.formIsValid()) {
+      this.loc8rDataService.addReviewByLocationId(this.location._id, this.newReview)
+        .then((review: Review) => {
+          console.log('Review Saved', review);
+          let reviews = this.location.reviews.slice(0);
+          reviews.unshift(review);
+          this.location.reviews = reviews;
+          this.resetAndHideReviewForm();
+        });
+    } else {
+      console.log('Not Valid.');
+      this.formError = 'All fields required, please try again.';
+    }
+  };
+
+  constructor(private loc8rDataService: Loc8rDataService) { }
+
+  ngOnInit() {
+  }
+
+}
